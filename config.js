17d1c1b1831ced275843e61f78da32606a879153
Uber Cardoso <hhcardoso@misena.edu.co>
2023-07-25 23:30:16 -0500

    add connection with database mysql

--- config.js
+++ config.js
@@ -1,1 +1,16 @@
 // fail of connections between environments and database
+export const PORT_SERVER = process.env.PORT_SERVER || 3010;
+
+const DB_PORT = process.env.DB_PORT || 3306;
+const DB_NAME = process.env.DB_NAME || "name";
+const DB_USER = process.env.DB_USER || "user";
+const DB_HOST = process.env.TZ_HOST || "localhost";
+const DB_PASSWORD = process.env.DB_PASSWORD || "password";
+
+export {
+  DB_PORT,
+  DB_NAME,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_USER,
+};
--- database/dbmysql.js
+++ database/dbmysql.js
@@ -1,1 +1,18 @@
 // connection to the mysql
+import { createPool } from "mysql2/promise.js";
+import { DB_PORT, DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } from "../config.js";
+
+// Database configuration
+const poll = createPool({
+  port: DB_PORT,
+  host: DB_HOST,
+  database: DB_NAME,
+  user: DB_USER,
+  password: DB_PASSWORD,
+});
+
+!poll
+  ? console.log("Not found connection")
+  : true;
+
+export { poll };
