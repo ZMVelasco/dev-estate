e1e15a6b0213f4a4489aeaa3b84f97388595a402
“Mariana <zaira.vl21@gmail.com>
2023-07-27 14:36:13 -0600

    Add ChakraUI

--- dev-estate/src/App.jsx
+++ dev-estate/src/App.jsx
@@ -4,21 +4,24 @@ import Home from './components/home/Home.jsx'
 import Login from './components/login/Login.jsx'
 import Catalog from './components/catalog/Catalog.jsx'
 import Admin from './components/admin/Admin.jsx'
+import { Flex } from '@chakra-ui/react'
 
 function App() {
 
   return (
       <div className='App'>
         <header>
-          <nav>
-            <ul>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/login'>Login</Link></li>
-              <li><Link to='/catalog'>Catalog</Link></li>
-              <li><Link to='/admin'>Admin</Link></li>
-            </ul>
-          </nav>
-        </header>
+      <nav>
+        <Flex flexDir="row">
+          <ul style={{ listStyle: 'none', padding: 0, margin: 0, display: 'flex' }}>
+            <li style={{ marginRight: '10px' }}><Link to='/'>Home</Link></li>
+            <li style={{ marginRight: '10px' }}><Link to='/login'>Login</Link></li>
+            <li style={{ marginRight: '10px' }}><Link to='/catalog'>Catalog</Link></li>
+            <li style={{ marginRight: '10px' }}><Link to='/admin'>Admin</Link></li>
+          </ul>
+        </Flex>
+      </nav>
+    </header>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/login' element={<Login/>} />
