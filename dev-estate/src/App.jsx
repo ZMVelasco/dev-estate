842447bdf9a957ebd399eda87b25e103f5f8852c
“Mariana <zaira.vl21@gmail.com>
2023-07-26 18:35:04 -0600

    Add header and nav

--- dev-estate/src/App.jsx
+++ dev-estate/src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Link } from 'react-router-dom'
 import Home from './components/home/Home.jsx'
 import Login from './components/login/Login.jsx'
 import Catalog from './components/catalog/Catalog.jsx'
@@ -9,7 +9,16 @@ function App() {
 
   return (
       <div className='App'>
-        Vite + React
+        <header>
+          <nav>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/login'>Login</Link></li>
+              <li><Link to='/catalog'>Catalog</Link></li>
+              <li><Link to='/admin'>Admin</Link></li>
+            </ul>
+          </nav>
+        </header>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/login' element={<Login/>} />
