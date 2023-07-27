f7124768ec6fc0f3d329329a021035e2ff15b2dd
“Mariana <zaira.vl21@gmail.com>
2023-07-26 18:11:57 -0600

    Add router

---
+++ dev-estate/src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/home/Home.jsx'
+import Login from './components/login/Login.jsx'
+import Catalog from './components/catalog/Catalog.jsx'
+import Admin from './components/admin/Admin.jsx'
+
+function App() {
+
+  return (
+      <div className='App'>
+        Vite + React
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/catalog' element={<Catalog/>} />
+          <Route path='/admin' element={<Admin/>} />
+        </Routes>    
+      </div>
+  )
+}
+
+export default App
