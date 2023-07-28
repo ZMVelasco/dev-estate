e0cd6f25205509e407df9b0e30426fea115d9759
“Mariana <zaira.vl21@gmail.com>
2023-07-28 17:31:39 -0600

    Remove ChakraUI, add Tailwind CSS

--- dev-estate/src/App.jsx
+++ dev-estate/src/App.jsx
@@ -1,34 +1,20 @@
-// import { useState } from 'react'
-import { Route, Routes, Link } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './components/home/Home.jsx'
 import Login from './components/login/Login.jsx'
 import Catalog from './components/catalog/Catalog.jsx'
 import Admin from './components/admin/Admin.jsx'
-import { Flex } from '@chakra-ui/react'
 
 function App() {
 
   return (
-      <div className='App'>
-        <header>
-      <nav>
-        <Flex flexDir="row">
-          <ul style={{ listStyle: 'none', padding: 0, margin: 0, display: 'flex' }}>
-            <li style={{ marginRight: '10px' }}><Link to='/'>Home</Link></li>
-            <li style={{ marginRight: '10px' }}><Link to='/login'>Login</Link></li>
-            <li style={{ marginRight: '10px' }}><Link to='/catalog'>Catalog</Link></li>
-            <li style={{ marginRight: '10px' }}><Link to='/admin'>Admin</Link></li>
-          </ul>
-        </Flex>
-      </nav>
-    </header>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/catalog' element={<Catalog/>} />
-          <Route path='/admin' element={<Admin/>} />
-        </Routes>    
-      </div>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/catalog' element={<Catalog />} />
+        <Route path='/admin' element={<Admin />} />
+      </Routes>
+    </div>
   )
 }
 
