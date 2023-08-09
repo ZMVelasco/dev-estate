3018e99e2c2be69d3c36d1deeb06b0f3882f90da
“Mariana <zaira.vl21@gmail.com>
2023-08-08 23:56:14 -0600

    Add signup component

--- dev-estate/src/App.jsx
+++ dev-estate/src/App.jsx
@@ -1,16 +1,18 @@
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/home/Home.jsx'
 import Login from './components/login/Login.jsx'
 import Catalog from './components/catalog/Catalog.jsx'
 import Admin from './components/admin/Admin.jsx'
+import Signup from './components/signup/Signup.jsx'
 
-function App() {
-
+function App () {
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
         <Route path='/catalog' element={<Catalog />} />
         <Route path='/admin' element={<Admin />} />
       </Routes>
