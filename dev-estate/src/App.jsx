bf69a5df5806a86d0308a6a271905f5ad271ddca
“Mariana <zaira.vl21@gmail.com>
2023-08-10 23:34:07 -0600

    Add assets, add filters component

--- dev-estate/src/App.jsx
+++ dev-estate/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './components/login/Login.jsx'
 import Catalog from './components/catalog/Catalog.jsx'
 import Admin from './components/admin/Admin.jsx'
 import Signup from './components/signup/Signup.jsx'
+import Filters from './components/home/Filters.jsx'
 
 function App () {
   return (
@@ -15,6 +16,7 @@ function App () {
         <Route path='/signup' element={<Signup />} />
         <Route path='/catalog' element={<Catalog />} />
         <Route path='/admin' element={<Admin />} />
+        <Route path='/filters' element={<Filters />} />
       </Routes>
     </div>
   )
